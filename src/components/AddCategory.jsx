be8f7ba75406592935181b32f44bbe067056106e
Aaron Quinteros <aaronroman3a@gmail.com>
2023-11-16 21:46:31 -0600

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const AddCategory = ( {onNewCategory} ) => {
+
+    const [InputValue, setInputValue] = useState('');
+
+    const onInputChange = ({ target }) => {
+        setInputValue( target.value );
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (InputValue.trim().length <= 1) return;
+        
+        onNewCategory(InputValue.trim());
+        setInputValue('');
+    };
+
+  return (
+    <form onSubmit={ onSubmit }>
+        <input
+            type="text"
+            placeholder="Buscar Gifs"
+            value={ InputValue }
+            onChange={ onInputChange }
+        />
+    </form>
+  )
+}
